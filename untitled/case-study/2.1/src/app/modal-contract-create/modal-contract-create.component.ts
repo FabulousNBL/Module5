bea49908b8703ed92ab8789d45b60ba087bd2264
FabulousNBL <nguyenblong2412@gmail.com>
2022-07-23 21:44:32 +0700

    upd

---
+++ untitled/case-study/2.1/src/app/modal-contract-create/modal-contract-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-contract-create',
+  templateUrl: './modal-contract-create.component.html',
+  styleUrls: ['./modal-contract-create.component.css']
+})
+export class ModalContractCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
