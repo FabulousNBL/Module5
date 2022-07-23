2ae0adf16a5ad26d79b5748f0f3deb579f3cee2c
FabulousNBL <nguyenblong2412@gmail.com>
2022-07-23 21:46:57 +0700

    upd

---
+++ untitled/case-study/2.1/src/app/contract.ts
@@ -0,0 +1,10 @@
+export interface Contract {
+  id: number;
+  startDay: string;
+  endDay: string;
+  customerId: number;
+  employeeId: number;
+  deposit: number;
+  total: number;
+  service: number;
+}
---
+++ untitled/case-study/2.1/src/app/customer.ts
@@ -0,0 +1,7 @@
+export interface Customer {
+  id: number,
+  name: string,
+  idCard: number,
+  phone: string,
+  type: number
+}
